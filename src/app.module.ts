174016066933045487a4790ae4367e1aefe9e34f
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-10-06 18:37:10 -0600

    Instalación prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BasicReportsModule } from './basic-reports/basic-reports.module';
 
 @Module({
-  imports: [],
+  imports: [BasicReportsModule],
   controllers: [],
   providers: [],
 })
